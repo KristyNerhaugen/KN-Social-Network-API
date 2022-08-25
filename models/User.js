23bac5030dd7cd6fc40a508393b7fb880a0049c1
kristynerhaugen <kristynerhaugen@gmail.com>
2022-08-25 16:42:15 -0500

    added to user model

--- models/User.js
+++ models/User.js
@@ -4,16 +4,24 @@ const { Schema, model } = require("mongoose");
 const UserSchema = new Schema({
   username: {
     type: String,
-    // add in unique
+    unique: true,
     required: true,
     trim: true,
   },
   email: {
     type: String,
-    // add in unique
+    unique: true,
     required: true,
     // must match to a valid email address--Mongoose's matching validation
   },
   //thoughts: {},
   //friends: {},
+});
+
+// virtual friendCount retrieves the length of the user's friends array
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.reduce(
+    (total, friend) => total + friend.replies.length + 1,
+    0
+  );
 });
