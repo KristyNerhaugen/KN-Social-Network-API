627e439296bb4102b09b7a812cd943410052ee9f
kristynerhaugen <kristynerhaugen@gmail.com>
2022-09-02 12:15:59 -0500

    set up create, update by id, and delete user routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -32,6 +32,34 @@ const userController = {
         res.sendStatus(400);
       });
   },
+  // create a User
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
+  // update User by ID
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user is found with this ID." });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  // delete a User by ID
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
 };
 
 module.exports = userController;
