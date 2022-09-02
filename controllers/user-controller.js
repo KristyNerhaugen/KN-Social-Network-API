5d19b2d18c7d8ce3e9d87c1b6382e15b82df4178
kristynerhaugen <kristynerhaugen@gmail.com>
2022-09-02 12:12:10 -0500

    set up get all and get one routes in user controller

---
+++ controllers/user-controller.js
@@ -0,0 +1,37 @@
+// import user model
+const { User } = require("../models");
+
+// userController for API routes
+const userController = {
+  // GET all users
+  getAllUser(req, res) {
+    User.find({})
+      .populate({
+        path: "thoughs",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+  // GET one user by ID
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+};
+
+module.exports = userController;
--- models/User.js
+++ models/User.js
@@ -2,29 +2,49 @@ const { Schema, model } = require("mongoose");
 
 // TO DO! require date format file
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      // must match to a valid email address--used https://mongoosejs.com/docs/api.html#schema_Schema-indexes
+      email: { type: String, set: toLower, required: true, unique: true },
+      // if above doesn't work, try this code taken from from https://thewebdev.info/2022/03/16/how-to-validate-email-syntax-with-mongoose/
+      // validate: [validateEmail, "Please fill a valid email address"],
+      // match: [
+      //   /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      //   "Please fill a valid email address",
+      // ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        re: "User",
+      },
+    ],
   },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    // must match to a valid email address--used https://mongoosejs.com/docs/api.html#schema_Schema-indexes
-    email: { type: String, set: toLower, required: true, unique: true },
-    // if above doesn't work, try this code taken from from https://thewebdev.info/2022/03/16/how-to-validate-email-syntax-with-mongoose/
-    // validate: [validateEmail, "Please fill a valid email address"],
-    // match: [
-    //   /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-    //   "Please fill a valid email address",
-    // ],
-  },
-  //thoughts: [{}],
-  //friends: [{}],
-});
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    // prevent virtuals from creating duplicate
+    id: false,
+  }
+);
 
 // virtual friendCount retrieves the length of the user's friends array
 UserSchema.virtual("friendCount").get(function () {
